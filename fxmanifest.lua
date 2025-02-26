2df2dc7cac17c64b2b682190ac8776c19591b7b5
Sr.Asorey <braismasorey@gmail.com>
2025-02-26 18:00:15 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sr.Asorey'
+description 'Sistema para entrar y salir de servicio'
+
+version '1.0'
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+dependencies {
+	'ox_target',
+	'qb-core'
+}
